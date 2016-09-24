c3fd1f28b13a19e928ae07be17b6c404ea30b4bc
Jurgen Feuchter <jfeuchter@gmail.com>
2016-09-24 12:04:46 -0500

    Added model taxes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924170326) do
+ActiveRecord::Schema.define(version: 20160924170413) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -118,6 +118,13 @@ ActiveRecord::Schema.define(version: 20160924170326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["address_id"], name: "index_stores_on_address_id"
+  end
+
+  create_table "taxes", force: :cascade do |t|
+    t.string   "name"
+    t.float    "percentage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "vendors", force: :cascade do |t|
