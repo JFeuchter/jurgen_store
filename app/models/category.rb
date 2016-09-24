63d874181e6bba88809096d3e59adcbe86f98c5c
Jurgen Feuchter <jfeuchter@gmail.com>
2016-09-24 14:53:50 -0500

    Cambio en models scope

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+	#scope :electronics, -> {find_by_name("Electronics")}
+	#crear de forma dinamica metodos
+	distinct.map{|c| c.name}.each do |v| 
+		self.class.class_eval do
+			define_method v.downcase.split(" ").join("_") do 
+				find_by_name(v)
+			end
+		end
+	end
 end
--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,2 +1,10 @@
 class ProductType < ApplicationRecord
+	#crear de forma dinamica metodos
+	distinct.map{|c| c.name}.each do |v| 
+		self.class.class_eval do
+			define_method v.downcase.split(" ").join("_") do 
+				find_by_name(v)
+			end
+		end
+	end
 end
