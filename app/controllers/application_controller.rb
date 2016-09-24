63804ab7ca92e85cf469c9ea26f82e0e838edbaa
Jurgen Feuchter <jfeuchter@gmail.com>
2016-09-24 15:26:58 -0500

    added select to forms helpers and change index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_categories
+  before_action :set_categories, :set_product_types, :set_vendors
 
   private 
   
   def set_categories
     @categories = Category.all
+  end
+  def set_product_types
+    @product_types = ProductType.all
+  end
+  def set_vendors
+    @vendors = Vendor.all
   end
 end
