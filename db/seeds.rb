1964a988bf1c6de28c39249475136c83b9ef2985
Jurgen Feuchter <jfeuchter@gmail.com>
2016-09-24 13:01:31 -0500

    Added test case seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+=begin
+Category.destroy_all
+ProductType.destroy_all
+Vendor.destroy_all
+Tax.destroy_all
+=end
+electronic = Category.create name: "Electronics"
+book = Category.create name: "Books"
+movie = Category.create name: "Movies"
+clothes = Category.create name: "Clothes"
+
+ProductType.create name: "Phone", category_id: electronic.id
+ProductType.create name: "Laptops", category_id: electronic.id
+ProductType.create name: "Tablets", category_id: electronic.id
+
+ProductType.create name: "Horror", category_id: book.id
+ProductType.create name: "Fiction", category_id: book.id
+ProductType.create name: "Sex", category_id: book.id
+
+ProductType.create name: "Horror", category_id: movie.id
+ProductType.create name: "Comedy", category_id: movie.id
+ProductType.create name: "Action", category_id: movie.id
+
+ProductType.create name: "T-Shirts", category_id: clothes.id
+ProductType.create name: "Shorts", category_id: clothes.id
+ProductType.create name: "Jeans", category_id: clothes.id
+
+addr = Address.create street: "Rio Guadalquivir", external_number: "422", country: "Mexico", neighborhood: "Del Valle", cp: "63710", state: "Nuevo Leon", city: "Monterrey" 
+
+Vendor.create name: "Nike"
+Vendor.create name: "Apple"
+Vendor.create name: "Editorial" 
+
+Warehouse.create name: "Almacen 1"
+Warehouse.create name: "Almacen 2"
+
+Tax.create name: "IVA"
+Tax.create name: "ISR"
+
+Store.create name: "Jurgen's Store"
