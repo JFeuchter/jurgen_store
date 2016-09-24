a86ea81801161ef270ca8ae16346e37947b70263
Jurgen Feuchter <jfeuchter@gmail.com>
2016-09-24 12:18:46 -0500

    Added category_id to product type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924171455) do
+ActiveRecord::Schema.define(version: 20160924171826) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -90,8 +90,10 @@ ActiveRecord::Schema.define(version: 20160924171455) do
 
   create_table "product_types", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_product_types_on_category_id"
   end
 
   create_table "product_warehouses", force: :cascade do |t|
